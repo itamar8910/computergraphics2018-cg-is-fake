30b7e178d3b996109855f2f7e1336f42cdc4d0b6
itamar8910 <itamar8910@gmail.com>
2018-07-17 19:35:14 +0300

    initialize orthogonal projection

--- Viewer/src/Camera.cpp
+++ Viewer/src/Camera.cpp
@@ -10,3 +10,14 @@ Camera::Camera()
 Camera::~Camera()
 {
 }
+
+void Camera::Ortho( const float left, const float right,
+                    const float bottom, const float top,
+                    const float zNear, const float zFar ){
+    
+    //initialization is column-wise
+    projection[0] = glm::vec4( 2 / (right-left), 0, 0, 0);
+    projection[1] = glm::vec4(0, 2 / (top-bottom), 0, 0);
+    projection[2] = glm::vec4( 0, 0, (-2) / (zFar - zNear), 0);
+    projection[3] = glm::vec4(-((right+left) / (right-left)), -((top+bottom) / (top-bottom)), -(zFar+zNear) / (zFar-zNear), 1);
+}
