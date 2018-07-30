43533e9a6ff8cd894ac50dd85f63c14a3dc6d816
Tomer Keren <tomer.keren.dev@gmail.com>
2018-07-30 14:10:46 +0300

    [WIP] Draw vertex normals (incorrectly)

--- Viewer/src/PrimMeshModel.cpp
+++ Viewer/src/PrimMeshModel.cpp
@@ -5,16 +5,3 @@ PrimMeshModel::PrimMeshModel(const vector<vector<glm::vec3> >& _triangles) : Mes
     triangles = _triangles;
     initializeInternals();
 }
-
-PrimMeshModel PrimMeshModel::CreatePyramid(){
-        vector<glm::vec3> face1 = {glm::vec3(0, 0, 0), glm::vec3(1, 0, 0), glm::vec3(0, 1, 0)};
-        vector<glm::vec3> face2 = {glm::vec3(0, 0, 0), glm::vec3(1, 0, 0), glm::vec3(0.5, 0.5, 1)};
-        vector<glm::vec3> face3 = {glm::vec3(0, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0.5, 0.5, 1)};
-        vector<glm::vec3> face4 = {glm::vec3(1, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0.5, 0.5, 1)};
-        vector<vector<glm::vec3> > triangles = {face1, face2, face3, face4};
-        PrimMeshModel pyramid = PrimMeshModel(); // for some reason getting a link error when trying to call 
-                                                 //PrimMeshModel(const vector<vector<glm::vec3> >& _triangles) constructor
-        pyramid.triangles = triangles;
-        pyramid.initializeInternals();
-        return pyramid;
-}
