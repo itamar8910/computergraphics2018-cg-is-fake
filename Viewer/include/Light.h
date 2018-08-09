c0ef1f0fc84c42d4273f1128419da669566225ea
Tomer Keren <tomer.keren.dev@gmail.com>
2018-08-09 12:20:42 +0300

    [WIP] Add light class hierchy

--- Viewer/include/Light.h
+++ Viewer/include/Light.h
@@ -3,9 +3,36 @@
 /*
  * Light class. Holds light source information and data.
  */
+typedef float intensity_t;
+typedef glm::vec3 color_t;
 class Light
 {
 public:
-	Light();
-	~Light();
+  Light(glm::vec3 location, intensity_t intensity) : intensity(intensity){};
+  ~Light();
+  intensity_t intensity;
+  glm::vec3 location;
+  virtual color_t applyOn(const color_t &color, const glm::vec3 normal) = 0;
 };
+
+class AmbientLight: public Light
+{
+  AmbientLight(intensity_t intensity) : Light(intensity){};
+  virtual color_t applyOn(const color_t &color, const glm::vec3 normal);
+};
+
+color_t AmbientLight::applyOn(const color_t &color, const glm::vec3 normal)
+{
+	return color * intensity;
+}
+
+class DiffuseLight: public Light
+{
+  DiffuseLight(intensity_t intensity) : Light(intensity){};
+  virtual color_t applyOn(const color_t &color, const glm::vec3 normal);
+};
+
+color_t DiffuseLight::applyOn(const color_t &color, const glm::vec3 normal)
+{
+	return color * intensity;
+}
