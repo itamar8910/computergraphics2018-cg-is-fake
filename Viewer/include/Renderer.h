5a2ef65d7a81b0c3e8c2ab48d246bc800195444e
Tomer Keren <tomer.keren.dev@gmail.com>
2018-09-22 20:50:37 +0300

    :fire: Remove dead Renderer functions

--- Viewer/include/Renderer.h
+++ Viewer/include/Renderer.h
@@ -49,26 +49,11 @@ private:
 
 	glm::mat4x4 getViewport();
 	// Draw's a pixel in location p with color color
-	void putPixel(int i, int j, float z, const glm::vec3 &color,bool clear=false);
-	glm::vec3 getPixel(int i, int j) const;
-	void putIModelIndex(int i, int j, int model_i);
-	void putZBufferval(int i, int j, int z);
-	float getZBufferVal(int i, int j);
-	// creates float array of dimension [3,w,h]
-	void createBuffers(int w, int h);
-	void DrawLineHelper(const glm::vec3 &point1, const glm::vec3 &point2
-						, const glm::vec3 &color= glm::vec3(0.0, 0.0, 0.0), int model_i=-1);
 
-	void DrawTriangle(const triangle3d_t &triangle, int model_i=-1, bool uniform_material = true, vector<color_t> vertices_ambient = NULL_VERTICES_COLOR, vector<color_t> vertices_diffusive = NULL_VERTICES_COLOR, vector<color_t> vertices_specular = NULL_VERTICES_COLOR);
-
-	//##############################
-	//##openGL stuff. Don't touch.##
-	//##############################
 	GLuint glScreenTex;
 	GLuint glScreenVtc;
 	void createOpenGLBuffer();
 	void initOpenGLRendering();
-	//##############################
 public:
 	int* model_i_buffer; // for each point, stores model index that's in that point or -1
 
@@ -95,15 +80,6 @@ public:
 
 	void DrawModel(GLuint vertexBufferID, GLuint normalsBufferID, GLuint uvBufferID, GLuint textureID, bool hasTexture, int num_of_triangles);
 
-	// Draws wireframe triangles to the color buffer
-	void DrawTriangles(const vector<triangle3d_t> &triangles, int model_i = -1, bool uniform_material = true,
-					vector<vector<color_t>> vertices_ambient = vector<vector<color_t>>(),
-					vector<vector<color_t>> vertices_diffusive = vector<vector<color_t>>(),
-					vector<vector<color_t>> vertices_specular = vector<vector<color_t>>());
-
-	void DrawLine(const glm::vec3 &point1, const glm::vec3 &point2, const glm::vec3 &color = glm::vec3(0.0, 0.0, 0.0), int model_i = -1);
-	
-	void scanFill(const triangle3d_t &triangle, const triangle3d_t &triangleWorld, int model_i = -1, bool uniform_material = true, vector<color_t> vertices_ambient = NULL_VERTICES_COLOR, vector<color_t> vertices_diffusive = NULL_VERTICES_COLOR, vector<color_t> vertices_specular = NULL_VERTICES_COLOR);
 	glm::vec3 TransformPoint(const glm::vec3 &originalPoint) const;
 	glm::vec3 ApplyObjectTransform(const glm::vec3 &originalPoint) const;
 	// Sets the camera transformations with relation to world coordinates
@@ -119,24 +95,8 @@ public:
 	void setLights(glm::vec3 ambient_color_light, vector<Light*>& lights);
 	void setFog(color_t color, bool enabled);
 
-	glm::vec3 calc_color_shade(const glm::vec3& location, const glm::vec3& normal, color_t ambient_color, color_t diffusive_color, color_t specular_color) const;
-
-	// Swaps between the back buffer and front buffer, as explained in class.
-	// https://en.wikipedia.org/wiki/Multiple_buffering#Double_buffering_in_computer_graphics
-	void SwapBuffers();
-
-	// Sets the color buffer to a new color (all pixels are set to this color).
-	void ClearColorBuffer(const glm::vec3& color);
-
 	// Resize the buffer.
 	void Viewport(int w, int h);
 
-	// Clears the z buffer to zero.
-	void ClearDepthBuffer();
-
-	// Draw wide vertical and horizontal lines on the screen
-	void SetDemoBuffer();
-
 	void set_supersampling_coeff(float _coeff);
-	void resampleColorBuffer();
 };
