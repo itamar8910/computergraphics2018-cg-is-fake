69dbcbb478a0c6901fedc6cf8bb52e43baf6bd8b
itamar8910 <itamar8910@gmail.com>
2018-10-08 16:30:51 +0300

    fix textures
    previously, was blurry
    now is gut

--- Viewer/src/MeshModel.cpp
+++ Viewer/src/MeshModel.cpp
@@ -188,6 +188,7 @@ void MeshModel::fillGLBuffers(){
 		// Give the image to OpenGL
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->texture_width, this->texture_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &this->texture_img[0]);
 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
